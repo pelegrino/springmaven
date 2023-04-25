fe82f01e1ceaf76b01b13ed7cac268b7ad3cd635
Pelegrino <pelegrino@gmail.com>
2023-04-25 08:20:27 -0300

    Criando END-POINT de listar todos usuários

--- src/main/java/br/com/pelegrino/springmaven/controllers/GreetingsController.java
+++ src/main/java/br/com/pelegrino/springmaven/controllers/GreetingsController.java
@@ -1,13 +1,19 @@
 package br.com.pelegrino.springmaven.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.pelegrino.springmaven.model.Usuario;
 import br.com.pelegrino.springmaven.repository.UsuarioRepository;
 
 @RestController
@@ -25,5 +31,15 @@ public class GreetingsController {
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
         return "Hello " + name + "!";
+    }
+    
+    @GetMapping(value = "listatodos")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> listaUsuario() {
+    	
+    	List<Usuario> usuarios = usuarioRepository.findAll();
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+    	
     }
 }
